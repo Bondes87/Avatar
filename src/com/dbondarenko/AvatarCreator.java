edc860ecb39c5933a7fe36b801c07f0c916d3277
Dmitro Bondarenko <bondes87@gmail.com>
2017-08-09 22:22:40 +0300

    Refactoring code.

--- src/com/dbondarenko/AvatarCreator.java
+++ src/com/dbondarenko/AvatarCreator.java
@@ -8,103 +8,88 @@ import java.io.IOException;
 
 /**
  * File: AvatarCreator.java
+ * The class that creates the avatar from the name.
  * Created by Dmitro Bondarenko on 09.08.2017.
  */
 public class AvatarCreator {
+    // The constant adjusting the size of the avatar (the number of pixels).
     private static final int SIZE_AVATAR = 5;
+    // The constant adjusting the background color of the avatar.
     private static final Color COLOR_BACKGROUND = new Color(255, 255, 255);
 
+    /**
+     * Create an avatar using the input string.
+     *
+     * @param name The input string.
+     */
     public void create(String name) {
-        int hashOfName = Math.abs(name.hashCode());
-        String bitSequence = Integer.toBinaryString(hashOfName);
-        //boolean[][] colorPixelAvatar = new boolean[SIZE_AVATAR][SIZE_AVATAR];
-        Color colorPixel = determineColorOfAvatar(hashOfName);
+        // Get the hash code from the string.
+        int hashCodeOfName = Math.abs(name.hashCode());
+        // Get the length of the bit sequence of the string.
+        int bitSequenceLength = Integer.toBinaryString(hashCodeOfName).length();
+        // Get avatar color.
+        Color colorOfAvatar = getColorOfAvatar(hashCodeOfName);
+        // Create image.
         BufferedImage bufferedImage = new BufferedImage(SIZE_AVATAR, SIZE_AVATAR, BufferedImage.TYPE_INT_RGB);
+        // Bit counter.
         int counter = 0;
         for (int i = 0; i < bufferedImage.getHeight(); i++) {
             for (int j = 0; j <= bufferedImage.getWidth() / 2; j++) {
-                int positionOfBit = counter % bitSequence.length();
-                if ((hashOfName >> positionOfBit & 1) == 1) {
-                    setColorPixel(colorPixel, bufferedImage, i, j);
+                int positionOfBit = counter % bitSequenceLength;
+                if ((hashCodeOfName >> positionOfBit & 1) == 1) {
+                    // Set pixel color.
+                    setColorPixel(colorOfAvatar, bufferedImage, j, i);
                 } else {
-                    setColorPixel(COLOR_BACKGROUND, bufferedImage, i, j);
-                }
-                /*colorPixelAvatar[i][j] = (hashOfName >> positionOfBit & 1) == 1;
-                if (j != colorPixelAvatar[0].length / 2) {
-                    colorPixelAvatar[i][colorPixelAvatar[0].length - 1 - j] = (hashOfName >> positionOfBit & 1) == 1;
-                }*/
-                counter++;
-            }
-        }
-        /*for (int i = 0; i < colorPixelAvatar.length; i++) {
-            for (int j = 0; j < colorPixelAvatar[0].length; j++) {
-                if (colorPixelAvatar[i][j]) {
-                    bufferedImage.setRGB(j, i, COLOR_BACKGROUND.getRGB());
-                } else {
-                    bufferedImage.setRGB(j, i, colorPixel.getRGB());
-                }
-            }
-        }*/
-       /* for (boolean[] ar : colorPixelAvatar) {
-            System.out.println(Arrays.toString(ar));
-        }*/
-        writeToFile(bufferedImage);
-
-        /*int hashOfName = name.hashCode();
-        String bitSequence = Integer.toBinaryString(hashOfName);
-        System.out.println(bitSequence);
-        boolean[][] colorPixelAvatar = new boolean[SIZE_AVATAR][SIZE_AVATAR];
-        int[] colorPixel = determineColorOfAvatar(hashOfName);
-        BufferedImage image = new BufferedImage(SIZE_AVATAR, SIZE_AVATAR, BufferedImage.TYPE_INT_RGB);
-        WritableRaster raster = image.getRaster();
-        int[] white = {255, 255, 255, 0};
-        int counter = 0;
-        for (int i = 0; i < colorPixelAvatar.length; i++) {
-            for (int j = 0; j <= colorPixelAvatar[0].length / 2; j++) {
-                colorPixelAvatar[i][j] = (hashOfName >> (counter % bitSequence.length()) & 1) == 1;
-                raster.setPixel(j, i, colorPixel = (hashOfName >> (counter % bitSequence.length()) & 1) == 1 ? colorPixel : white);
-                if (j != colorPixelAvatar[0].length / 2) {
-                    // System.out.println(hashOfName >> (counter % bitSequence.length()) & 1);
-                    colorPixelAvatar[i][colorPixelAvatar[0].length - 1 - j] = (hashOfName >> (counter % bitSequence.length()) & 1) == 1;
-                    raster.setPixel(colorPixelAvatar[0].length - 1 - j, i, colorPixel = (hashOfName >> (counter % bitSequence.length()) & 1) == 1 ? colorPixel : white);
+                    // Set pixel color.
+                    setColorPixel(COLOR_BACKGROUND, bufferedImage, j, i);
                 }
                 counter++;
             }
         }
-        for (boolean[] ar : colorPixelAvatar) {
-            System.out.println(Arrays.toString(ar));
-        }
-
-        File outputfile = new File("image.jpg");
-        try {
-            ImageIO.write(image, "jpg", outputfile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-*/
+        // Write image to file.
+        writeImageToFile(bufferedImage);
     }
 
-    private void writeToFile(BufferedImage bufferedImage) {
-        File image = new File("avatar.png");
-        try {
-            ImageIO.write(bufferedImage, "png", image);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void setColorPixel(Color colorPixel, BufferedImage bufferedImage, int i, int j) {
-        bufferedImage.setRGB(j, i, colorPixel.getRGB());
-        if (j != bufferedImage.getWidth() / 2) {
-            bufferedImage.setRGB(bufferedImage.getWidth() - 1 - j, i, colorPixel.getRGB());
-        }
-    }
-
-    private Color determineColorOfAvatar(int hashOfName) {
+    /**
+     * Get the avatar color using the hash code of the string.
+     *
+     * @param hashOfName The hash code of the string.
+     * @return Color of avatar.
+     */
+    private Color getColorOfAvatar(int hashOfName) {
         int red = hashOfName % 256;
         int green = (red * 2) % 256;
         int blue = (red * 3) % 256;
         return new Color(red, green, blue);
     }
 
+    /**
+     * Set pixel color.
+     *
+     * @param colorPixel    The color of pixel.
+     * @param bufferedImage The image that changes the color of the pixel.
+     * @param x             The pixel coordinate.
+     * @param y             The pixel coordinate.
+     */
+    private void setColorPixel(Color colorPixel, BufferedImage bufferedImage, int x, int y) {
+        bufferedImage.setRGB(x, y, colorPixel.getRGB());
+        if (SIZE_AVATAR % 2 == 0 || x != bufferedImage.getWidth() / 2) {
+            bufferedImage.setRGB(bufferedImage.getWidth() - 1 - x, y, colorPixel.getRGB());
+        }
+    }
+
+    /**
+     * Writes the resulting image to a file.
+     *
+     * @param bufferedImage The Image to write to file.
+     */
+    private void writeImageToFile(BufferedImage bufferedImage) {
+        File image = new File("avatar.png");
+        try {
+            ImageIO.write(bufferedImage, "png", image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/com/dbondarenko/Main.java
+++ src/com/dbondarenko/Main.java
@@ -2,11 +2,12 @@ package com.dbondarenko;
 
 /**
  * File: Main.java
+ * The program that creates avatars. The class that runs the program.
  * Created by Dmitro Bondarenko on 09.08.2017.
  */
 public class Main {
     public static void main(String[] args) {
         AvatarCreator avatar = new AvatarCreator();
-        avatar.create("Bogdan");
+        avatar.create("Dmitro");
     }
 }
